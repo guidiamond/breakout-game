22812f47b6e399822137be11a9f496f9bb2090b5
guidiamond <guilherme.batista770@gmail.com>
2021-03-02 03:12:20 -0300

    breakout working with minor issues

---
+++ Assets/_Scripts/UI_Menu_Principal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Menu_Principal : MonoBehaviour
+{
+    GameManager gm;
+
+    private void OnEnable()
+    {
+        gm = GameManager.GetInstance();
+    }
+
+    public void Comecar()
+    {
+        Debug.Log("Carai");
+        gm.ChangeState(GameManager.GameState.GAME);
+    }
+}
