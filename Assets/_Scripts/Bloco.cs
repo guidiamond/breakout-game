1fbf25d466b5c6ebdf17d102026ffcd4f2753f39
guidiamond <guilherme.batista770@gmail.com>
2021-03-03 01:10:24 -0300

    projeto nota 8

--- Assets/_Scripts/Bloco.cs
+++ Assets/_Scripts/Bloco.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Bloco : MonoBehaviour
 {
+    public int Durability { get; set; }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        Durability -= 1;
+        if (Durability <= 0) {
+           Destroy(gameObject);
+        }
     }
     // Start is called before the first frame update
     void Start()
     {
-
+        Durability = Random.Range(1, 3);
     }
 
     // Update is called once per frame
